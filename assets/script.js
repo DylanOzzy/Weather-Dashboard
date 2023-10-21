a45778f18cc6f44ad1842a80ea2219ecfcb9c792
unknown <dylanosborn123@hotmail.com>
2023-10-21 12:02:36 -0500

    Added Java Script that enables current date and time shown at top of page, also Java script for seach history

--- assets/script.js
+++ assets/script.js
@@ -11,4 +11,30 @@ function displayTimeDate () {
 
 displayTimeDate ();
 
+$("#searchBtn").on("click", function (event) {
+    event.preventDefault();
+    $('#searchCity').html();
+    localStorage.setItem("Search", $('#searchCity').val());
+    let searchHistory = JSON.parse(localStorage.getItem("SearchHistory")) || [];
+    const newSearch = $('#searchCity').val();
+    searchHistory.push(newSearch);
+    localStorage.setItem("SearchHistory", JSON.stringify(searchHistory));
+    displaySearchHistory(searchHistory);
+});
+
+function displaySearchHistory(searchHistory) {
+    $('#displaySearchHistory').empty();
+    
+    for (const search of searchHistory) {
+        const listItem = $("<li>").text(search);
+        $('#displaySearchHistory').prepend(listItem);
+    }
+}
+
+$(document).ready(function () {
+    const searchHistory = JSON.parse(localStorage.getItem("SearchHistory")) || [];
+    displaySearchHistory(searchHistory);
+});
+
+
 
